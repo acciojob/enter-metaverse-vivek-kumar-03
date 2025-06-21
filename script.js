fed1039bbd45f4a0bc78830997374461ad294917
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-21 21:16:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function () {
+  const statusElement = document.getElementById("status");
+  statusElement.innerHTML = "<h1>Entered Metaverse</h1>";
+});
+
